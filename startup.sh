9cceabe0fe51572f5150eb594f54dc3a19877e35
Juliano Teixeira <julianozero@gmail.com>
2021-07-08 10:02:35 -0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -79,7 +79,7 @@ docker --version
 sudo chmod 777 /var/run/docker.sock
 
 echo 'installing docker-compose' 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
